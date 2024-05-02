5a46fff0e3f6138a08bac3626dd148f24a3a65d3
hiroaki-oohata <li0n0831@yahoo.co.jp>
2024-05-03 01:37:28 +0900

    New

---
+++ code/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    printf("Hello,World!\n");
+    printf("sizeof(size_t)=%ld\n", sizeof(size_t));
+    return 0;
+}
---
+++ code/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: Shift-JIS -*-
+
+# 数当てゲーム
+# ランダムに決められた数字（1～100）を当てる
+
+import random as rd
+
+# 正解時の画面表示処理
+def disp_msg(count):
+    msg = ""
+    if count < 5 :
+        msg = "Wonderful!"
+    elif count < 10 :
+        msg = "Great!"
+    else:
+        msg = "Good!"
+
+    print(msg ,"正解です")
+    return
+
+# メイン処理
+print("**** 数当てゲーム ****")
+print("1-100の数字を入力してください。end入力で終了")
+
+ans = rd.randint(1,100)
+#print("[DBG] ans=",ans)
+
+cnt = 0
+while True:
+    num = input("数字を入力してください > ")
+    if num == "end":
+        break
+    num = int(num)
+    if num < ans :
+        print("・・・もっと大きい")
+    elif ans < num :
+        print("・・・もっと小さい")
+    elif ans == num :
+        disp_msg(cnt)
+        break
+    else:
+        print("[ERR] num=",num)
+    cnt = cnt + 1
